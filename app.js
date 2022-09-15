05a8c768cabcf49b1f7a378d29d895c01e5e3a66
feliperjj <felipeoliveirabento@gmail.com>
2022-09-14 22:41:21 -0300

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -128,31 +128,13 @@ app.post("/work", function (req, res) {
   res.redirect("/");
 });
 
+let port = process.env.PORT;
+if (port ==null || port ==""){
+
+    port = 3000;
+};
+
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server Running");
 });
 
-// switch(currentDay){
-//     case 0:
-//         day = "Sunday";
-//         break;
-//     case 1:
-//         day ="Monday";
-//         break;
-//     case 2:
-//         day = "Tuesday";
-//     break;
-//     case 3:
-//     day = "Wednesday";
-//     break;
-//     case 4:
-//    day="Tuesday";
-//    break;
-//    case 5:
-//     day="Friday";
-//    break;
-//    case 6:
-//     day="Saturday";
-//     break;
-//     default:
-//         console.log("Error");
